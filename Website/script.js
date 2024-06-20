d601cbb94f5841d22efcac125cf1138a2c09bc02
Supriya <samantsupriya335@gmail.com>
2024-06-20 22:18:22 +0530

    first commit

---
+++ Website/script.js
@@ -0,0 +1,10 @@
+const clickOpen = document.getElementById('btn1');
+const dailog = document.getElementById('dialogbtn1');
+function clickme(){
+    dialogbtn1.click();
+}
+const clickOpen1= document.getElementById('btn2');
+const dailog1 = document.getElementById('dialogbtn12');
+function clickme1(){
+    dialogbtn2.click();
+}
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask ,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    #return 'Hello, World!'
+
+@app.route('/products')
+def product():
+    return 'My Prodeucts!'
+
+if __name__ == "__main__":
+    app.run(debug = True)
